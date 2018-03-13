df53d81583424f4d7d23815e892353f7d0505314
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-03-13 22:37:58 +0300

    I don't need that HttpHeaders

--- client/src/app/teams/team.service.ts
+++ client/src/app/teams/team.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Team } from './team.model';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
