327aeaf24c26d1b478d910a662880d2b8f7e4232
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-03-21 23:30:49 +0300

    Display skaters on team page

--- client/src/app/teams/team-detail/team-detail.component.ts
+++ client/src/app/teams/team-detail/team-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Team } from '../team.model';
 import { TeamService } from '../team.service';
+import { Skater } from '../../skaters/skater.model';
+import { SkaterService } from '../../skaters/skater.service';
 
 
 @Component({
@@ -11,14 +13,18 @@ import { TeamService } from '../team.service';
 })
 export class TeamDetailComponent implements OnInit {
   team: Team | null = null;
+  skaters: Skater[] = [];
 
   constructor(private route: ActivatedRoute,
-              private teamService: TeamService) {}
+              private teamService: TeamService,
+              private skaterService: SkaterService) {}
 
   ngOnInit() {
     const abbrev = this.route.snapshot.paramMap.get('abbrev');
     this.teamService.getTeam(abbrev)
       .subscribe(team => this.team = team);
+    this.skaterService.searchSkaters({'team_abbrev': abbrev})
+      .subscribe(skaters => this.skaters = skaters);
   }
 
 }
