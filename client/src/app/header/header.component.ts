58341a8ad115fd0333dfd00c07496e50f027150b
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-03-26 23:09:32 +0300

    Better header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Suggestion } from '../suggestions/suggestion.model';
 import { SuggestionService } from '../suggestions/suggestion.service';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,13 +12,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() shadow: any;
   suggestions: Suggestion[] = [];
 
   constructor(
+    private renderer: Renderer2,
     private router: Router,
     private suggestionService: SuggestionService
   ) { }
-  
+
   getSuggestions(name) {
     if (name === '') {
       this.suggestions = [];
@@ -26,11 +30,22 @@ export class HeaderComponent {
         .subscribe(suggestions => this.suggestions = suggestions)
     }
   }
-  
+
   clearSuggestionsAndNavigate(input, destination) {
     input.value = '';
     this.suggestions = [];
     this.router.navigate(destination);
   }
-  
+
+  activateSearching(nav) {
+    this.renderer.addClass(nav, 'searching');
+    this.renderer.addClass(this.shadow, 'main-shadow_activated');
+  }
+
+  deactivateSearching(nav) {
+    this.renderer.removeClass(nav, 'searching');
+    this.renderer.removeClass(this.shadow, 'main-shadow_activated');
+    this.suggestions = [];
+  }
+
 }
